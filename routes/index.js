7772468f3fd66d6cf20ddaa4c153db595e305bd6
Chris Cheng <gemini2015@hotmail.com>
2018-01-18 01:05:41 -0800

    add model

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var AppPackage = require('../models/app_package')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  	res.render('index', { title: 'App Report' });
 });
 
 module.exports = router;
